238b45b5f5911d515a084290ca01a220f8b61756
highlandproger <magashdaha@mail.ru>
2024-06-13 01:01:40 +0300

    Bug fix with Terms parse mode

--- src/main/java/ru/rusguardian/bot/command/main/menu_commands/TermsCommand.java
+++ src/main/java/ru/rusguardian/bot/command/main/menu_commands/TermsCommand.java
@@ -2,6 +2,7 @@ package ru.rusguardian.bot.command.main.menu_commands;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -9,6 +10,8 @@ import ru.rusguardian.bot.command.service.Command;
 import ru.rusguardian.bot.command.service.CommandMapping;
 import ru.rusguardian.bot.command.service.CommandName;
 import ru.rusguardian.domain.user.Chat;
+
+import javax.swing.text.html.HTML;
 
 @Component
 @RequiredArgsConstructor
@@ -29,6 +32,7 @@ public class TermsCommand extends Command {
                 .text(getTextByViewDataAndChatLanguage(VIEW_DATA, chat.getAiSettingsEmbedded().getAiLanguage()))
                 .chatId(chat.getId())
                 .disableWebPagePreview(true)
+                .parseMode(ParseMode.HTML)
                 .build();
         sendMessage(message);
     }
