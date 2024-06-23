f30cda5702b958d41f97b273a3edf7fe6b77e3ef
highlandproger <magashdaha@mail.ru>
2024-06-23 12:00:23 +0300

    UPDATE to 1.11.0 Add GetKnown for image. Update Util to 1.1.0 (Obtain getViewFromCallbackButton)

--- src/main/java/ru/rusguardian/bot/command/prompts/image/img/inner/GetKnownForImageCommand.java
+++ src/main/java/ru/rusguardian/bot/command/prompts/image/img/inner/GetKnownForImageCommand.java
@@ -1,17 +1,25 @@
 package ru.rusguardian.bot.command.prompts.image.img.inner;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.rusguardian.bot.command.prompts.PromptCommand;
 import ru.rusguardian.bot.command.service.CommandName;
 import ru.rusguardian.domain.user.Chat;
 import ru.rusguardian.service.ai.constant.AIModel;
+import ru.rusguardian.service.process.prompt.ProcessPromptVision;
+import ru.rusguardian.telegram.bot.util.util.FileUtils;
 import ru.rusguardian.telegram.bot.util.util.TelegramUtils;
 
 @Component
+@RequiredArgsConstructor
 public class GetKnownForImageCommand extends PromptCommand {
+
+    private final ProcessPromptVision processPromptVision;
+
     @Override
     public CommandName getType() {
         return CommandName.GET_KNOWN_FOR_IMAGE;
@@ -21,15 +29,20 @@ public class GetKnownForImageCommand extends PromptCommand {
     protected void mainExecute(Update update) throws TelegramApiException {
 
         Chat chatOwner = getChatOwner(update);
-        if (isChatLimitExpired(chatOwner, AIModel.STABLE_DIFFUSION)) {
+
+        if (isChatLimitExpired(chatOwner, AIModel.GPT_4_OMNI)) {
             edit(getEditMessageWithResponse(TelegramUtils.getChatId(update), getChatLimitExpiredString(chatOwner), TelegramUtils.getMessageId(update)));
             return;
         }
 
-        AnswerCallbackQuery answer = new AnswerCallbackQuery();
-        answer.setCallbackQueryId(TelegramUtils.getCallbackQueryId(update));
-        answer.setText("Пока еще в разработке");
-        answer.setShowAlert(true);
-        bot.execute(answer);
+        String prompt = TelegramUtils.getCallbackViewMessage(update);
+        String initImageUrl = FileUtils.getFileUrlFromMessage(((Message) update.getCallbackQuery().getMessage()).getReplyToMessage(), bot).toString();
+        Chat chat = getChatOwner(update);
+        processPromptVision.process(chat, initImageUrl, prompt)
+                .thenAccept(response -> editForPrompt(EditMessageText.builder().chatId(chat.getId()).text(response).messageId(TelegramUtils.getMessageId(update)).build()))
+                .exceptionally(e -> {
+                    errorCommand.execute(update);
+                    throw new RuntimeException(e);
+                });
     }
 }
